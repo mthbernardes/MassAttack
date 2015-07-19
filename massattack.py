41cfb20ce069b926af67d06f3f90505f652ed1e5
Matheus Bernardes <mthbernardes@gmail.com>
2015-07-19 05:22:42 -0300

    Update massattack.py
    Melhorias na detecção de fabricante

--- massattack.py
+++ massattack.py
@@ -22,39 +22,42 @@ def pegaip():
 				print bcolors.OKBLUE+'[+] - Endereco: '+result+bcolors.ENDC
 				checaip(result)
 def checaip(result):
-	ip = 'http://'+result
-	c = requests.get(ip)
-	print bcolors.WARNING+'[+] - 1 - Tentativa de encontrar fabricante'+bcolors.ENDC
-	if c.headers['www-authenticate']: 
-		server = c.headers['www-authenticate'].split("=")[1].split('"')[1]
-		print bcolors.OKGREEN+'[+] - 1 - Fabricante encontrado '+server+bcolors.ENDC
-		fabricante = open('fabricantes/'+server+'.txt','a')
-		fabricante.write(ip+'\n')
-		print
-
-	else:	
-		print bcolors.WARNING+'[+] - 2 - Tentativa de encontrar fabricante'+bcolors.ENDC
-		value = c.text.find("<title>")
-		if value != -1:
-			server = c.text[value:-1].split()[0].split('>')[1].split('<')[0]
-			print bcolors.OKGREEN+'[+] - 2 - Fabricante encontrado '+server+bcolors.ENDC
+	try:
+		ip = 'http://'+result
+		c = requests.get(ip,timeout=0.3)
+		print bcolors.WARNING+'[+] - 1 - Tentativa de encontrar fabricante'+bcolors.ENDC
+		if c.headers['www-authenticate']: 
+			server = c.headers['www-authenticate'].split("=")[1].split('"')[1]
+			print bcolors.OKGREEN+'[+] - 1 - Fabricante encontrado '+server+bcolors.ENDC
 			fabricante = open('fabricantes/'+server+'.txt','a')
 			fabricante.write(ip+'\n')
 			print
-		else:
-			print bcolors.WARNING+'[+] - 3 - Tentativa de encontrar fabricante'+bcolors.ENDC
-			value = c.text.find("<TITL>")
+
+		elif not c.headers['www-authenticate']:
+			print bcolors.WARNING+'[+] - 2 - Tentativa de encontrar fabricante'+bcolors.ENDC
+			value = c.text.find("<title>")
 			if value != -1:
+				c = requests.get(ip,timeout=0.3)
 				server = c.text[value:-1].split()[0].split('>')[1].split('<')[0]
-				print bcolors.OKGREEN+'[+] - 3 - Fabricante encontrado '+server+bcolors.ENDC
+				print bcolors.OKGREEN+'[+] - 2 - Fabricante encontrado '+server+bcolors.ENDC
 				fabricante = open('fabricantes/'+server+'.txt','a')
 				fabricante.write(ip+'\n')
 				print
-
 			else:
-				print bcolors.FAIL+'[+] - Fabricante nao encontrado, salvo em semregistro.txt'+bcolors.ENDC
-				semregistro = open('fabricantes/semregistro.txt','a')
-				semregistro.write(ip+'\n')
-				print
+				c = requests.get(ip,timeout=0.3)
+				print bcolors.WARNING+'[+] - 3 - Tentativa de encontrar fabricante'+bcolors.ENDC
+				value = c.text.find("<TITL>")
+				if value != -1:
+					server = c.text[value:-1].split()[0].split('>')[1].split('<')[0]
+					print bcolors.OKGREEN+'[+] - 3 - Fabricante encontrado '+server+bcolors.ENDC
+					fabricante = open('fabricantes/'+server+'.txt','a')
+					fabricante.write(ip+'\n')
+					print
+
+	except:
+		print bcolors.FAIL+'[+] - Fabricante nao encontrado, salvo em semregistro.txt'+bcolors.ENDC
+		semregistro = open('fabricantes/semregistro.txt','a')
+		semregistro.write(ip+'\n')
+		print
 
 pegaip()
